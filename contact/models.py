2b7d83a296e839791a3576a7301814cde21efab0
Julio César <julioprattes@gmail.com>
2026-09-12 16:05:33 -0300

    Criando Models para o banco de dados

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class ContactModel(models.Model):
+    first_name = models.CharField( max_length=100)
+    last_name = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)                    
+    # category = models.ForeignKey()
+    # show = models.BooleanField()
+    # owner = models.ForeignKey()
+    # picture = models.ImageField()
