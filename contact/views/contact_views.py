14800534cff34c00a936d54bcd8be7ba06cc2e18
Julio César <julioprattes@gmail.com>
2026-09-16 22:25:50 -0300

    Manipulando QuerySets

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from contact.models import ContactModel
 
 def index_view(request):
-    contact = ContactModel.objects.all()  
-
-    # .filter(show=True).order_by('-id')[]
+    contact = ContactModel.objects \
+    .all().filter(show=True).order_by('-id')[:10]
 
     return render(
         request,
