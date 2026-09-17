ec95a7c26139e17f019cadd57078166e2a4c063b
Julio César <julioprattes@gmail.com>
2026-09-16 22:48:37 -0300

    criando view e a url

--- contact/urls.py
+++ contact/urls.py
@@ -5,4 +5,5 @@ app_name = 'contact'
 
 urlpatterns = [
     path('',views.index_view, name='index'),
+    path('<int:contact_id>/', views.contact_views, name='contact'),
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from contact.models import ContactModel
 
 def index_view(request):
@@ -8,5 +9,21 @@ def index_view(request):
     return render(
         request,
         'contact/index.html',
-        {'contacts': contact}
+        {'contact': contact}
+    )
+
+
+def contact_view(request, contact_id):
+    # contact = ContactModel.objects \
+    # .filter(id=contact_id).first()
+    contact = get_object_or_404(
+        ContactModel,
+        id=contact_id,
+        show=True
+    )
+
+    return render(
+        request,
+        'contact/contact.html',
+        {'contact': contact}
     )
